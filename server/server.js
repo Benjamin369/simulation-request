9b5413d87396a4504ce6df12d83693833dacab49
Benjamin <benjamin@ismylover.cn>
2021-07-09 15:13:32 +0800

    检查params参数

--- server/server.js
+++ server/server.js
@@ -8,17 +8,27 @@ app.post('/*', (req, res) => {
 })
 function doUrl (req, res, type) {
     // 地址URL
+    console.log('req', req)
     let url = req._parsedUrl.pathname
-    console.log('is xhr',JSON.stringify(req.headers));
     let urlInfo = request.cheakUrl(url, type)
-    if (urlInfo.length < 1){
+    if (!urlInfo.url){
         res.status(404)
         res.end('')
         return
     }
-    // if 
-    res.status(404)
-    res.end('')
-
+    let header = req.headers
+    if (!request.cheakToken(header, urlInfo)) {
+        res.status(401)
+        res.end('')
+        return
+    }
+    let params = req.query
+    let returnMessage = request.cheakparams(params, urlInfo)
+    res.status(200)
+    let returnInfo = null
+    if (returnMessage) {
+        returnInfo = request.getErrorReturn(returnMessage)
+    }
+    res.json(returnInfo)
 }
 const server = app.listen(3000)
--- util/request.js
+++ util/request.js
@@ -8,13 +8,50 @@ request.prototype.cheakUrl = function(url, type) {
     })
     if (infoArray && infoArray.length > 0) {
         return infoArray[0]
-    }else {
-        return []
+    }else if (infoArray && infoArray.url) {
+        return infoArray
+    }else{
+        return {}
     }
 }
 // 检查Token
 request.prototype.cheakToken = function(header, info) {
+    if (info.skipToken) {
+        return true
+    }
     let requestToken = header.token
-    
+    if (info.token === requestToken) {
+        return true
+    }
+    if (!info.token && requestToken === baseData.defaultToken) {
+        return true
+    }
+    return false
+},
+// 检查params
+request.prototype.cheakparams = function(params, info) {
+    let paramsRule = info.params
+    for (let key in paramsRule) {
+        if (paramsRule[key].required && !params[key]) {
+            return "[ " + key + ' ] 不能为空'
+        }
+    }
+}
+request.prototype.getErrorReturn = function(message) {
+    let returnObj = {}
+    let returnObjRuls = baseData.returnObj
+    for (let key in returnObjRuls) {
+        let row = returnObjRuls[key]
+        if (row.type === 'requestStatus') {
+            returnObj[key] = row.error
+        }
+        if (row.type === 'returnObj') {
+            returnObj[key] = row.default
+        }
+        if (row.type === 'message') {
+            returnObj[key] = message || row.default
+        }
+    }
+    return returnObj
 }
 module.exports = new request();
